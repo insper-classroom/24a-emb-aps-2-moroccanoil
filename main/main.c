3aa0c3d3631a4b320b75543f589a4e721167d564
Mariana <marianaaa1@al.insper.edu.br>
2024-04-25 15:46:13 +0000

    funcionando com bluetooth

--- main/main.c
+++ main/main.c
@@ -1,4 +1,3 @@
-
 #include <FreeRTOS.h>
 #include <task.h>
 #include <semphr.h>
@@ -13,12 +12,6 @@
 #include <math.h>
 #include <stdlib.h>
 
-// const int HC06_UART_ID = 1;
-// const int HC06_BAUD_RATE = 9600;
-
-// const int HC06_TX_PIN = 4;
-// const int HC06_RX_PIN = 5;
-
 const int BUTTON_JUMP_RED_PIN = 21;
 const int BUTTON_SWITCH_BLUE_PIN = 16;
 
@@ -28,8 +21,6 @@ const int LED_GREEN_PIN = 22;
 
 const int ADC0_X = 26; //x
 const int ADC0_CHANNEL = 0;
-// const int ADC1_Y = 27; //y
-// const int ADC1_CHANNEL = 1;
 
 int volatile btn_axis = 0;
 int volatile btn_value = 0;
@@ -54,133 +45,136 @@ int adjust_scale(int adc_value) {
     }
 }
 
-// void btn_callback(){
-//     if (gpio_get(BUTTON_JUMP_RED_PIN) == 1){
-//         int btn_red = 1;
-//         xQueueSend(xQueueBtn, &btn_red, portMAX_DELAY);
-//     }
-//     if (gpio_get(BUTTON_SWITCH_BLUE_PIN) == 1){
-//         int btn_axis = 0;
-//         xQueueSend(xQueueBtn, &btn_axis, portMAX_DELAY);
-//     }
-//     else if (gpio_get(BUTTON_SWITCH_BLUE_PIN) == 0){
-//         int btn_axis = 1;
-//         xQueueSend(xQueueBtn, &btn_axis, portMAX_DELAY);
-//     }
-// }
-
-void btn_callback(uint gpio, uint32_t events){
+void btn_callback(uint gpio, uint32_t events) {
     adc_t data;
-    if (events & GPIO_IRQ_EDGE_FALL){
-        if (gpio == BUTTON_JUMP_RED_PIN){
-        data.val = 3;
-        data.axis = btn_axis;
-    }
-    
-    if (gpio == BUTTON_SWITCH_BLUE_PIN){
-        btn_axis = !btn_axis;
-        data.val = 0;
-    }
+    if (events & GPIO_IRQ_EDGE_FALL) {
+        if (gpio == BUTTON_JUMP_RED_PIN) {
+            data.val = 3;
+            data.axis = btn_axis;
+        }
+
+        if (gpio == BUTTON_SWITCH_BLUE_PIN) {
+            btn_axis = !btn_axis;
+            data.val = 0;
+            }
         
     }
 
-    if (events & GPIO_IRQ_EDGE_RISE){
-        if (gpio == BUTTON_JUMP_RED_PIN){
-        data.val = 0;
-        data.axis = btn_axis;
+    if (events & GPIO_IRQ_EDGE_RISE) {
+        if (gpio == BUTTON_JUMP_RED_PIN) {
+            data.val = 0;
+            data.axis = btn_axis;
+        }
     }
-    }
-        
-    
+
     xQueueSend(xQueueAdc, &data, portMAX_DELAY);
-    // xQueueSendFromISR(xQueueAdc, &data, portMAX_DELAY); 
-    
-    // else if (gpio_get(BUTTON_SWITCH_BLUE_PIN) = 0){
-    //     data.axis = 1;
-    //     btn_axis = 1;
-    //     xQueueSend(xQueueAdc, &data, portMAX_DELAY);
-    // }
 }
-
-
 
 void write_package(adc_t data) {
     int val = data.val;
     int msb = val >> 8;
-    int lsb = val & 0xFF ;
+    int lsb = val & 0xFF;
 
-    uart_putc_raw(uart0, data.axis); 
-    uart_putc_raw(uart0, lsb);
-    uart_putc_raw(uart0, msb); 
-    uart_putc_raw(uart0, -1); 
-    // printf("Data: %d\n", data.val);
-    
-}
+    uart_putc_raw(uart1, data.axis);
+    uart_putc_raw(uart1, lsb);
+    uart_putc_raw(uart1, msb);
+    uart_putc_raw(uart1, -1);
+
+    // uart_putc_raw(uart0, data.axis);
+    // uart_putc_raw(uart0, lsb);
+    // uart_putc_raw(uart0, msb);
+    // uart_putc_raw(uart0, -1);
+
+ }
 
 void x_task(void *p) {
     adc_t data;
-    // if (gpio_get(BUTTON_JUMP_RED_PIN) == 0){
-    //     data.val = 0;
-    //     data.axis = btn_axis;
-    //     xQueueSend(xQueueAdc, &data, portMAX_DELAY);
-    // }
-    // if (gpio_get(BUTTON_SWITCH_BLUE_PIN) == 1){
-    //     btn_axis = !btn_axis;}
-        
+
     adc_init();
     adc_gpio_init(ADC0_X);
 
+    int last_value = 0;
+    xQueueSend(xQueueAdc, &data, portMAX_DELAY);
+    
+
     int v[5];
-    //int mediaX;
 
     while (1) {
         adc_select_input(ADC0_CHANNEL);
-        v[0]=v[1];
-        v[1]=v[2];
-        v[2]=v[3];
-        v[3]=v[4];
-        v[4]=adc_read();
-        int mediaX = ((v[4]+v[3]+v[2]+v[1]+v[0])/5);
+        v[0] = v[1];
+        v[1] = v[2];
+        v[2] = v[3];
+        v[3] = v[4];
+        v[4] = adc_read();
+        int mediaX = ((v[4] + v[3] + v[2] + v[1] + v[0]) / 5);
 
         int coisa = adjust_scale(adc_read());
 
-        if(coisa > 100){ 
+        if (coisa > 220) {
             data.val = 2;
             data.axis = btn_axis;
-            xQueueSend(xQueueAdc, &data, portMAX_DELAY);
-        } else if (coisa < -100){
+            
+                xQueueSend(xQueueAdc, &data, portMAX_DELAY);
+                last_value = data.val;
+        }
+            
+        else if (coisa < -220) {
             data.val = 1;
-            data.axis = btn_axis; 
-            xQueueSend(xQueueAdc, &data, portMAX_DELAY);
-        } else {
+            data.axis = btn_axis;
+            
+                xQueueSend(xQueueAdc, &data, portMAX_DELAY);
+             
+        }
+        else {
             data.val = 0;
             data.axis = btn_axis;
-            xQueueSend(xQueueAdc, &data, portMAX_DELAY);
+            
+                xQueueSend(xQueueAdc, &data, portMAX_DELAY);
+                
+            
         }
 
-        vTaskDelay(pdMS_TO_TICKS(100));
-    }
+        vTaskDelay(pdMS_TO_TICKS(50));
 
+    }
 }
 
-// void btn_task() {
-//     adc_t data;
-//     data.val = btn_value;
-//     data.axis = btn_axis;
-//     xQueueSend(xQueueAdc, &data, portMAX_DELAY);
-// }
+
+void led_task(void *p) {
+
+    while (1) {
+        
+            if (btn_axis == 0) {
+                // Acende led azul
+                gpio_put(LED_BLUE_PIN, 1);
+                gpio_put(LED_RED_PIN, 0);
+            }
+
+            if (btn_axis == 1) {
+                // Acende led vermelho
+                gpio_put(LED_RED_PIN, 1);
+                gpio_put(LED_BLUE_PIN, 0);
+            }
+        }
+    
+}
 
 void uart_task(void *p) {
+    static int uart_counter = 0; // Contador para controlar chamadas da uart_task
     adc_t data;
     int btn_red = 0;
-    // int btn_axis;
+
     while (1) {
         if (xQueueReceive(xQueueAdc, &data, portMAX_DELAY) == pdTRUE) {
-            write_package(data);
+            if (data.axis == 0 || data.axis == 1) {
+                write_package(data);
+            }
+        
+
+            
+            }
         }
     }
-}
-
 
 
 void hc06_task(void *p) {
@@ -188,10 +182,9 @@ void hc06_task(void *p) {
     uart_init(HC06_UART_ID, HC06_BAUD_RATE);
     gpio_set_function(HC06_TX_PIN, GPIO_FUNC_UART);
     gpio_set_function(HC06_RX_PIN, GPIO_FUNC_UART);
-    hc06_init("aps2_legal", "1234");
+    hc06_init("eu_tentei", "1234");
 
     while (true) {
-        uart_puts(HC06_UART_ID, "OLAAA ");
         vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
@@ -213,22 +206,19 @@ int main() {
 
     gpio_pull_up(BUTTON_JUMP_RED_PIN);
     gpio_pull_up(BUTTON_SWITCH_BLUE_PIN);
-    
-    gpio_set_irq_enabled_with_callback(BUTTON_JUMP_RED_PIN, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE , true, &btn_callback);
-                                    
-    gpio_set_irq_enabled(BUTTON_SWITCH_BLUE_PIN, GPIO_IRQ_EDGE_FALL, true);
 
-    // printf("Start bluetooth task\n");BUTTON_SWITCH_BLUE_PIN;
+    gpio_set_irq_enabled_with_callback(BUTTON_JUMP_RED_PIN, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &btn_callback);
+    gpio_set_irq_enabled(BUTTON_SWITCH_BLUE_PIN, GPIO_IRQ_EDGE_FALL, true);
 
     xQueueAdc = xQueueCreate(32, sizeof(adc_t));
     xQueueBtn = xQueueCreate(32, sizeof(uint64_t));
     xTaskCreate(hc06_task, "UART_Task 1", 4096, NULL, 1, NULL);
     xTaskCreate(x_task, "x_task", 4096, NULL, 1, NULL);
     xTaskCreate(uart_task, "uart_task", 4096, NULL, 1, NULL);
-    
-    
-    vTaskStartScheduler();
+    // Adicionando a criação da led_task aqui
+    xTaskCreate(led_task, "led_task", 4096, NULL, 1, NULL);
 
+    vTaskStartScheduler();
 
     while (true)
         ;
--- python/main.py
+++ python/main.py
@@ -2,8 +2,8 @@ import serial
 import uinput
 from time import sleep
 
-ser = serial.Serial('/dev/ttyACM0', 115200)
-# ser = serial.Serial('/dev/rfcomm0', 115200)
+# ser = serial.Serial('/dev/ttyACM0', 115200)
+ser = serial.Serial('/dev/rfcomm0', 115200)
 
 # Create new keyboard device
 device = uinput.Device([
@@ -24,58 +24,44 @@ def parse_data(data):
     return axis, value
 
 def press_key(axis, value):
-    if value == 1:
+    global last_value   
+    if value == 0:
+        device.emit(uinput.KEY_A, 0)
+        device.emit(uinput.KEY_LEFT, 0)
+        device.emit(uinput.KEY_D, 0)
+        device.emit(uinput.KEY_RIGHT, 0)
+        device.emit(uinput.KEY_W, 0)
+        device.emit(uinput.KEY_UP, 0)
+        
+    #indo para esquerda    
+    elif value == 1: 
         if value +10*axis == 1:
             device.emit(uinput.KEY_A, 1)
-            sleep(0.1)
-            device.emit(uinput.KEY_A, 0)
-        
-        if value +10*axis == 11:
-            device.emit(uinput.KEY_LEFT, 1)
             
-            device.emit(uinput.KEY_LEFT, 0)
-    
-    if value == 2:
+        elif value +10*axis == 11:
+            device.emit(uinput.KEY_LEFT, 1)
+                    
+    #indo para direita
+    elif value == 2:
         if value +10*axis == 2:
             device.emit(uinput.KEY_D, 1)
-            sleep(0.1)
-            device.emit(uinput.KEY_D, 0)
-        if value +10*axis == 12:
+           
+        elif value +10*axis == 12:
             device.emit(uinput.KEY_RIGHT, 1)
-            sleep(0.1)
-            device.emit(uinput.KEY_RIGHT, 0)
-    
+            
     #botao vermelho pula
-    if value == 3:
+    elif value == 3:
         if value +10*axis == 3:
             device.emit(uinput.KEY_W, 1)
-            sleep(0.1)
-            device.emit(uinput.KEY_W, 0)
-        
-        if value +10*axis == 13:
+            
+        elif value +10*axis == 13:
             device.emit(uinput.KEY_UP, 1)
-            sleep(0.1)
-            device.emit(uinput.KEY_UP, 0)
-
-
-# def move_mouse(axis, value):
-#     if axis == 0:    # X-axis
-#         device.emit(uinput.REL_X, value)
-#     elif axis == 1:  # Y-axis
-#         device.emit(uinput.REL_Y, value)
-
-def click_button(button):
-    if button == 0:
-        device.emit(uinput.BTN_LEFT, 1)
-        device.emit(uinput.BTN_LEFT, 0)
-    elif button == 1:
-        device.emit(uinput.BTN_RIGHT, 1)
-        device.emit(uinput.BTN_RIGHT, 0)
-
+        
+           
 try:
     # sync package
     while True:
-        print('Waiting for sync package...')
+        # print('Waiting for sync package...')
         while True:
             data = ser.read(1)
             if data == b'\xff':
